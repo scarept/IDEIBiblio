a66f21dda6bbaa7975840d77d18be1ff4bda505a
Márcio Martins <marciomartins.it@gmail.com>
2013-12-06 11:53:26 +0000

    no message

--- IDEIBiblio/Controllers/HomeController.cs
+++ IDEIBiblio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IDEIBiblio.Dal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,23 @@ namespace IDEIBiblio.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Listagem de produtos";
+            DataContext contexto = new IDEIBiblio.Dal.DataContext();
+            List<IDEIBiblio.Models.Livro> allProds = contexto.Livroes.ToList();
+            List<IDEIBiblio.Models.Livro> prod = new List<IDEIBiblio.Models.Livro>();
+            if (allProds.Count() > 4)
+            {
+                prod.Add(allProds.ElementAt(allProds.Count()-1));
+                prod.Add(allProds.ElementAt(allProds.Count() - 2));
+                prod.Add(allProds.ElementAt(allProds.Count() - 3));
+                prod.Add(allProds.ElementAt(allProds.Count() - 4));
 
-            return View();
+            }
+            else
+            {
+                prod = allProds;
+            }
+            return View(prod);
         }
 
         public ActionResult About()
