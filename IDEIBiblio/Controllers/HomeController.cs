7388e8c60abae0f796ee1605148c2e328efdb085
1111121RicardoBrandao <ricardombrandao@gmail.com>
2013-12-09 04:38:19 +0000

    Novidades is now a partial view

--- IDEIBiblio/Controllers/HomeController.cs
+++ IDEIBiblio/Controllers/HomeController.cs
@@ -12,23 +12,8 @@ namespace IDEIBiblio.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Listagem de produtos";
-            DataContext contexto = new IDEIBiblio.Dal.DataContext();
-            List<IDEIBiblio.Models.Livro> allProds = contexto.Livroes.ToList();
-            List<IDEIBiblio.Models.Livro> prod = new List<IDEIBiblio.Models.Livro>();
-            if (allProds.Count() > 4)
-            {
-                prod.Add(allProds.ElementAt(allProds.Count()-1));
-                prod.Add(allProds.ElementAt(allProds.Count() - 2));
-                prod.Add(allProds.ElementAt(allProds.Count() - 3));
-                prod.Add(allProds.ElementAt(allProds.Count() - 4));
-
-            }
-            else
-            {
-                prod = allProds;
-            }
-            return View(prod);
+            
+            return View();
         }
 
         public ActionResult About()
